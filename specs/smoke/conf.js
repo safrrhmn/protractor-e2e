37b6c3b3346f27e0c37ac840ecd57e290f534e74
Saifur Rahman <safrrhmn@hotmail.com>
2015-03-30 20:45:13 -0500

    edited git ignore and JUnit xml reporter

--- specs/smoke/conf.js
+++ specs/smoke/conf.js
@@ -1,11 +1,12 @@
 'use strict';
+var jasmineReporters = require('jasmine-reporters');
 
 exports.config = {
     /* seleniumAddress: 'http://127.0.0.1:4444/wd/hub',
      ** enables to connect to the browsers directly.
      ** http://stackoverflow.com/questions/27889488/cannot-run-protractor-on-internet-explorer-11
      */
-
+    framework: 'jasmine2',
     directConnect: true,
     specs: ['giftengHomePageTests.js'],
 
@@ -24,11 +25,17 @@ exports.config = {
     ],
     jasmineNodeOpts: {
         showColors: true,
-        defaultTimeoutInterval: 30000,
-        isVerbose: true
+        defaultTimeoutInterval: 90000,
+        isVerbose: true,
+        includeStackTrace: true
     },
 
     onPrepare: function () {
+        jasmine.getEnv().addReporter(new jasmineReporters.JUnitXmlReporter({
+            savePath: './reports/',
+            consolidateAll: true
+        }));
+
         browser.driver.manage().window().maximize();
     }
 }
